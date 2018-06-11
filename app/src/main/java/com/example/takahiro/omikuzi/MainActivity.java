9f8d5f65e9d5f153ad41789327e99a3d986c1bcd
Takafoi <fujito.takahiro@gmail.com>
2018-06-11 19:33:12 +0900

    jsonできた

--- app/src/main/java/com/example/takahiro/omikuzi/MainActivity.java
+++ app/src/main/java/com/example/takahiro/omikuzi/MainActivity.java
@@ -20,8 +20,10 @@ import com.google.android.gms.location.places.Places;
 import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -40,7 +42,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     private final int PLACE_PICKER_REQUEST = 1;
 
     private Button button1, button2;
-    private ObjectMapper mapper = new ObjectMapper();
+//    private ObjectMapper mapper = new ObjectMapper();
 
     EditText et;
     Info info = new Info();
@@ -107,21 +109,51 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         button2 = (Button) findViewById(R.id.delete);
         TextView viewData = (TextView) findViewById(R.id.data);
 
+        String viewDay = null;
+        String viewPlace = null;
+        String viewResult = null;
+                
+
 
         try {
+            FileInputStream input = openFileInput("omikuji.json");
+            BufferedReader inputText = new BufferedReader(new InputStreamReader(input));
+
+            JSONArray jsona = new JSONArray(inputText.readLine());
+            JSONObject json = jsona.getJSONObject(0);
+
+
+            viewDay = json.getString("day");
+            viewPlace = json.getString("place");
+            viewResult = json.getString("result");
+
+
+            Log.d("aaa", viewDay);
+            Log.d("aaa", viewPlace);
+            Log.d("aaa", viewResult);
+
+            viewData.setText(viewDay + viewPlace + viewResult + "\n");
+
+
+//            et.append(viewDay + viewPlace + viewResult + "\n");
+//            et.setText(viewDay);
+            /*
             JsonNode node = mapper.readTree(new File("omikuji.json"));
 
             String view_day = node.get("day").asText();
             String view_place = node.get("place").asText();
             String view_result = node.get("result").asText();
 
+
             et.append(view_day + view_place + view_result + "\n");
             et.setText(view_day);
+            */
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
             e.printStackTrace();
         }
 
-        viewData.setText(et.getText());
 
 
 
@@ -211,7 +243,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
             BufferedReader reader = new BufferedReader( new InputStreamReader( in , "UTF-8") );
             String tmp2;
 
-            et.setText("");
+//            et.setText("");
 
             while( (tmp2 = reader.readLine()) != null ){
                 tmp=tmp + tmp2;
@@ -223,10 +255,11 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         }
 
         try {
-            Union = mapper.writeValueAsString(info);
+//            Union = mapper.writeValueAsString(info);
             FileOutputStream out = openFileOutput( "omikuji.json", MODE_PRIVATE );
-            out.write( tmp.getBytes());
-            out.write( Union.getBytes());
+//            out.write( tmp.getBytes());
+//            out.write( Union.getBytes());
+            out.write("[{\"day\":\"2018/06/08\",\"place\":\"日本、〒918-8231 福井県福井市問屋町３丁目６０９\",\"result\":\"だいきちぃ～\"}]".getBytes());
         } catch (IOException e) {
             e.printStackTrace();
         }
